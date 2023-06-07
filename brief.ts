f6f5987acfbff0389ae22c95a0f54577342652db
Gareth-Moore <gccsmoore3@gmail.com>
2023-06-07 09:10:16 +0100

    uploaded project brief file

---
+++ brief.ts
@@ -0,0 +1,44 @@
+/* 
+    main components
+
+    header
+    # logo
+    # Search bar
+    # theme toggle
+
+    sidebar
+    # filters games by Genre
+    # heading
+    # ul of genre
+
+    main / main content / display games
+    # big heading
+    # 2 select elements
+    # > select platform
+    # > select order by
+        game card
+        # image
+        # list of available platforms by icon
+        # rating
+        # name
+        # what appears to be a recommendation? Just an icon of thumbs up or arrow on target
+
+    component tree:
+
+    header 
+        > header container
+        > search bar
+        > theme toggle button
+    
+    sidebar
+        > sidebar container
+        > <li> for genres
+    
+    main
+        > main container
+        > select/filter container
+            > select by platform
+            > select by order by
+        > display game cards container
+            > card
+*/
